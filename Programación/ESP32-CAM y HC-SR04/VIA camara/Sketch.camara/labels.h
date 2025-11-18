876914b24a77ac4f34c7f19b75afa8f1a551b716
bautistapaz <104784684+bautistapaz@users.noreply.github.com>
2025-11-18 14:11:12 -0300

    carpeta con todo los labels.h y model.h iniciales

---
+++ Programación/ESP32-CAM y HC-SR04/VIA camara/Sketch.camara/labels.h
@@ -0,0 +1,7 @@
+#ifndef LABELS_H
+#define LABELS_H
+
+const char* labels[] = {"persona","perro","gato"};
+const int NUM_CLASSES = sizeof(labels)/sizeof(labels[0]);
+
+#endif // LABELS_H
---
+++ Programación/ESP32-CAM y HC-SR04/VIA camara/Sketch.camara/model.h
@@ -0,0 +1,8 @@
+// model.h de prueba mínima
+#ifndef MODEL_H
+#define MODEL_H
+
+const unsigned char model[] = { 0x20, 0x00, 0x00, 0x00 }; 
+const int model_len = sizeof(model);
+
+#endif
